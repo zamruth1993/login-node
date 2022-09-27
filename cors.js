b2e5d249bce4346f213565a198cfc8140d5f9b9e
zamruthbegum <zamruth.m.1993@gmail.com>
2022-09-27 08:09:32 +0530

    first commit

---
+++ cors.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const whitelist = ['http://localhost:4200'];
+
+var corsOptionDelegate = (req,callback) =>{
+    var corsOptions;
+     if(whitelist.indexOf(req.header('Origin'))!== -1){
+         corsOptions = {origin:true};
+     }
+     else{
+         corsOptions = {origin:false};
+     }
+     callback(null,corsOptions);
+}
+
+exports.cors = cors();
+app.use(cors({ origin: true })); 
+exports.corsWithOptions = cors(corsOptionDelegate);
